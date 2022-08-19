4122185dfb2473366b6fbf98f409a9bfa954e811
H_VK <hvkcoder@gmail.com>
2022-08-19 14:37:21 +0800

    ➕ 创建 ColumnList 组件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <ColumnList :list="list"/>
+  </div>
 </template>
 
 <script lang="ts">
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+
+const testData:ColumnProps[] = [{
+  id: 1,
+  title: 'test专栏1',
+  description: '这是一个描述',
+  avatar: 'https://avatars.githubusercontent.com/u/12862565?s=400&u=644d159c4fe5cf84c211befcb611f27a00391c54&v=4'
+},
+{
+  id: 2,
+  title: 'test专栏2',
+  description: '这是一个描述',
+  avatar: 'https://avatars.githubusercontent.com/u/12862565?s=400&u=644d159c4fe5cf84c211befcb611f27a00391c54&v=4'
+},
+{
+  id: 3,
+  title: 'test专栏3',
+  description: '这是一个描述'
+  // avatar: 'https://avatars.githubusercontent.com/u/12862565?s=400&u=644d159c4fe5cf84c211befcb611f27a00391c54&v=4'
+}]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
